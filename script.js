a05b7b17de17a6ff7caf72b7221ea3e8ead1fd06
bkylee <brian.ky.lee@outlook.com>
2022-05-12 00:10:53 -0700

    Started on script

---
+++ script.js
@@ -0,0 +1,14 @@
+const gridX = 4;
+const body = document.body;
+const grid = document.createElement('div');
+const row1 = document.createElement('div');
+const row2 = document.createElement('div');
+const row3 = document.createElement('div');
+const row4 = document.createElement('div');
+
+function createDivRow (rows) {
+    rows = rows; 
+    let row = document.createElement('div');
+    row.className = "row";
+    
+}
