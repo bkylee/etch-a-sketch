bd276cec41efc8fb38c98a58c659468190e92f9d
bkylee <brian.ky.lee@outlook.com>
2022-05-14 14:05:16 -0400

    made changes to html, css to create border and set display type to inline-block, javascript function for creating columns

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const gridX = 4;
+//set foundation variables and constants 
 const body = document.body;
-const grid = document.createElement('div');
+//container to hold grid in body
+const container = document.createElement('div');
+let rows = document.getElementsByClassName('rows');
+let cells = document.getElementsByClassName('cells');
 body.appendChild(grid);
 
-function createRow (columns) {
-    for (x = 0; x < columns ; x++) {
-        const box = document.createElement('div');
-        box.setAttribute('class', 'gridBox');
-        box.style.flex;
-        grid.appendChild(box);
+
+//function to create rows based on number of cells set in parameter
+function createRow (cells) {
+    for (x = 0; x < cells ; x++) {
+        const row = document.createElement('div');
+        container.appendChild(row).className = 'rows';
     }
 }
 
-function createGrid (rows, columns) {
-    for (x = 0; x < rows ; x ++) {
-        createRow(columns);
+function createColumns (numberCells) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < numberCells; j++) {
+            let cell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
     }
 }
-
-createGrid(4,4);
-
-
+}
