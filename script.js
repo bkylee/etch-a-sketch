c102715649b08551103f6e9e5f31031b8c4c48eb
bkylee <brian.ky.lee@outlook.com>
2022-05-17 17:53:47 -0400

    Redid code and changed querySelectorAll related vars and what not

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-//set foundation variables and constants 
 const body = document.body;
-//container to hold grid in body
 const container = document.createElement('div');
-let rows = document.getElementsByClassName('rows');
-let cells = document.getElementsByClassName('cell');
 body.appendChild(container);
+let rows = document.querySelectorAll('div.row');
+let cells = document.querySelectorAll('div.cell')
+container.append(rows);
+container.append(cells);
 
-//function to create rows based on number of numberRows set in parameter
-function createRow (numberRows) {
-    for (x = 0; x < numberRows ; x++) {
-        const row = document.createElement('div');
-        container.appendChild(row).className = 'rows';
+function createRow (num){
+    for (i = 0; i < num ; i ++) {
+        let row = document.createElement('div');
+        row.className('row');
+        container.appendChild('row');
     }
 }
 
-function createColumns (numberCells) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < numberCells; j++) {
-            let cell = document.createElement("div");
-            rows[j].appendChild(cell).className = "cell";
-            cell.addEventListener('hover', ()=>cell.background = "blue");
+function createCell(num) {
+    for (i = 0; i < num; i++) {
+        for (j = 0; j < num; j++){
+            let cell = document.createElement('div');
+            cell.addEventListener('hover', ()=> cell.style.background = "black");
+           rows[j].appendChild(cell).className('cell');
+        }
     }
-}
 }
 
 function grid (userInput = 4) {
-createRow(userInput);
-createColumns(userInput);
+    createCell(userInput);
+    createCell(userInput);
 }
 
 grid();
