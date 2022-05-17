d1538a5efc8f2a8d32b31700cf69662f95620835
bkylee <brian.ky.lee@outlook.com>
2022-05-17 18:18:06 -0400

    Changed appendChild things by removing single quotes

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ container.append(cells)
 function createRow (num){
     for (i = 0; i < num ; i ++) {
         let row = document.createElement('div');
-        row.className('row');
-        container.appendChild('row');
+        row.classList.add('row');
+        container.appendChild(row);
     }
 }
 
 function createCell(num) {
     for (i = 0; i < num; i++) {
         for (j = 0; j < num; j++){
-            let cell = document.createElement('div').
-            cell.className('cell').addEventListener('hover', ()=> cell.style.background = "black");
-           rows[j].appendChild('.cell');
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            //cell.addEventListener('hover', ()=> cell.style.background = "black");
+           rows[j].appendChild(cell);
         }
     }
 }
 
 function grid (userInput = 4) {
-    createCell(userInput);
+    createRow(userInput);
     createCell(userInput);
 }
 
