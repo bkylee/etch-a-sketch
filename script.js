4c041c391814e50831fca6c1d01869bd792214a4
bkylee <brian.ky.lee@outlook.com>
2022-05-29 11:10:40 -0400

    Changed resetGrid to work on all new grids by adding new selector for function

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ container.appendChild(reset);
 
 
 function resetGrid (){
-        for (i = 0; i < cells.length; i ++){
-        cells[i].style.background = "white";
+    const newCells = document.getElementsByClassName('cell');
+        for (i = 0; i < newCells.length; i ++){
+        newCells[i].style.background = "white";
 }
 }
-//let randomColour = Math.floor(Math.random()*16777215).toString(16);
 
 
 //button to create a new grid based on number entered
@@ -26,10 +26,14 @@ container.appendChild(gridNumber);
 
 function newGrid(){
     x = prompt("Please enter a number greater than 0 to set the X and Y axis for the new grid.");
+
+    //get all elements with "cell" class and convert nodelist to an Array to iterate over and remove from DOM 
     cells = Array.from(document.getElementsByClassName('cell'));
     cells.forEach(element => {
         element.remove();
     });
+    //check if input is a number and is greater than 0
+    //return new grid based on entered number
     if (x < 0 || isNaN(x)) {
         return newGrid();
     }
@@ -60,7 +64,7 @@ function createCell(num) {
     }
 }
 
-
+//default grid is 16x16
 function grid (userInput = 16) {
     createRow(userInput);
     createCell(userInput);
